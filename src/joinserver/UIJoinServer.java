b9af5afd86c27aa9b5cdb6533be00f62400fd775
Marco <Marco@172.16.105.185>
2015-11-20 10:28:38 +0100

    secondo commit

---
+++ src/joinserver/UIJoinServer.java
@@ -0,0 +1,13 @@
+package joinserver;
+
+/**
+ *
+ * @author Damiano Di Stefano, Marco Giuseppe Salafia
+ */
+public class UIJoinServer
+{
+    public static void main(String[] args)
+    {
+        
+    }
+}
